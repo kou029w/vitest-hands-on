494ca30619906866fdc5a71d2418a3ed2e6fe532
Kohei Watanabe <kou029w@gmail.com>
2022-02-24 21:50:24 +0900

    fix test name

--- templates/promise/promise.test.js
+++ templates/promise/promise.test.js
@@ -3,21 +3,21 @@
 // ダミー
 const fetchData = () => Promise.resolve("peanut butter");
 
-test("the data is peanut butter", () => {
+test("the data is peanut butter - return promise", () => {
   return fetchData().then((data) => {
     expect(data).toBe("peanut butter");
   });
 });
 
-test("the data is peanut butter", () => {
+test("the data is peanut butter - return promise with resolves", () => {
   return expect(fetchData()).resolves.toBe("peanut butter");
 });
 
-test("the data is peanut butter", async () => {
+test("the data is peanut butter - async/await", async () => {
   const data = await fetchData();
   expect(data).toBe("peanut butter");
 });
 
-test("the data is peanut butter", async () => {
+test("the data is peanut butter - async/await with resolves", async () => {
   await expect(fetchData()).resolves.toBe("peanut butter");
 });
