f04d46ef86ff16ebd63b7d9e47e8ec3c5302a515
Kohei Watanabe <nebel@fogtype.com>
2026-09-12 21:32:48 +0900

    Refactor mock test to call mockCallback in tests

--- templates/mock/mock.test.js
+++ templates/mock/mock.test.js
@@ -2,12 +2,12 @@ import { expect, test, vi } from "vitest";
 
 const mockCallback = vi.fn();
 
-["い", "ろ", "は"].forEach(mockCallback);
-
 test("関数は3回呼ばれます", () => {
+  ["い", "ろ", "は"].forEach(mockCallback);
   expect(mockCallback.mock.calls.length).toBe(3);
 });
 
 test("最初の呼び出しのときの第1引数は「い」です", () => {
+  ["い", "ろ", "は"].forEach(mockCallback);
   expect(mockCallback.mock.calls[0][0]).toBe("い");
 });
